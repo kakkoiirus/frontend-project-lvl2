98818d34526a49c1f1349de61db674ba7bd7e0fe
Valentin <bagadin@inbox.ru>
2021-01-09 17:09:08 +0500

    Init

---
+++ src/bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import commander from 'commander';
+const program = commander.program;
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.');
+
+program.parse(process.argv);
