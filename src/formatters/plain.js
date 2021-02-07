9c6707129f163b6bc9c2010f34ce61d15eb49d58
Valentin <bagadin@inbox.ru>
2021-02-08 01:09:22 +0500

    Refactoring

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -22,7 +22,7 @@ export default (diff) => {
   const iter = (ast, path = []) => {
     const changedItems = ast.filter(({ status }) => status !== 'unchanged');
 
-    const result = changedItems.flatMap((item) => {
+    const result = changedItems.map((item) => {
       const {
         key,
         value,
@@ -38,17 +38,18 @@ export default (diff) => {
         return iter(value, newPath);
       }
 
-      let resultString = `Property '${newPath.join('.')}' was ${STATUS_MAP[status]}`;
+      const resultString = [];
+      resultString.push(`Property '${newPath.join('.')}' was ${STATUS_MAP[status]}`);
 
       if (status === 'updated') {
-        resultString += `. From ${stringify(oldValue)} to ${stringify(value)}`;
+        resultString.push(`. From ${stringify(oldValue)} to ${stringify(value)}`);
       }
 
       if (status === 'added') {
-        resultString += ` with value: ${stringify(value)}`;
+        resultString.push(` with value: ${stringify(value)}`);
       }
 
-      return resultString;
+      return resultString.join('');
     });
 
     return result.join('\n');
