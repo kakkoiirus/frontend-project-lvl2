0b124b0066e0dcb32f66f49961988b3e74cbe446
Valentin <bagadin@inbox.ru>
2021-02-12 11:13:28 +0500

    Improve naming

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,27 +1,27 @@
 import path from 'path';
 import fs from 'fs';
 import parse from './parsers.js';
-import ast from './ast.js';
-import format from './formatters/index.js';
+import buildAst from './ast.js';
+import formater from './formatters/index.js';
 
 const readFile = (filepath) => {
   const fullPath = path.resolve(process.cwd(), filepath);
 
-  const file = fs.readFileSync(fullPath, 'utf-8');
+  const data = fs.readFileSync(fullPath, 'utf-8');
 
-  return file;
+  return data;
 };
 
-const getFormatOfFile = (filepath) => path.extname(filepath).slice(1);
+const getFileFormat = (filepath) => path.extname(filepath).slice(1);
 
-export default (filepath1, filepath2, formaterName = 'stylish') => {
-  const formatOfFile1 = getFormatOfFile(filepath1);
-  const formatOfFile2 = getFormatOfFile(filepath2);
+export default (filepath1, filepath2, format = 'stylish') => {
+  const fileFormat1 = getFileFormat(filepath1);
+  const fileFormat2 = getFileFormat(filepath2);
 
-  const parsedFile1 = parse(readFile(filepath1), formatOfFile1);
-  const parsedFile2 = parse(readFile(filepath2), formatOfFile2);
+  const parsedData1 = parse(readFile(filepath1), fileFormat1);
+  const parsedData2 = parse(readFile(filepath2), fileFormat2);
 
-  const buildedAst = ast(parsedFile1, parsedFile2);
+  const ast = buildAst(parsedData1, parsedData2);
 
-  return format(buildedAst, formaterName);
+  return formater(ast, format);
 };
